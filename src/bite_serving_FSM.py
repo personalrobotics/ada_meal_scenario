53348dd35df6dfec5215b9e7330e71b762ae1730
Stefanos Nikolaidis <snikolai@andrew.cmu.edu>
2015-04-02 14:43:29 -0400

    fixed spaces

--- src/bite_serving_FSM.py
+++ src/bite_serving_FSM.py
@@ -61,191 +61,177 @@ defaultEndEffectorPose = np.asarray([[-0.00947845,  0.02440905, -0.99965712,  0.
                                           [ 0.17490731,  0.9843306 ,  0.0223764 ,  1.10983745],
                                           [ 0.        ,  0.        ,  0.        ,  1.        ]])
 
-
-
 class ADAmanipulationTester:
   
   #Finite State Machine
   ROBOT_STATE = "INITIAL"
   plateDetectedTimes = 1
   def addWaterServingTask(self):
-      self.waterServing_task = self.tasklist.add_task('WaterServing')
-      self.findGlass_subtask = self.waterServing_task.add_task('Find Glass')
-      self.graspGlass_subtask = self.waterServing_task.add_task('Grasp Glass')
-      self.liftGlass_subtask = self.waterServing_task.add_task('Lift Glass')
-      self.drinkGlass_subtask = self.waterServing_task.add_task('Drink Glass')
-      self.returnGlass_subtask = self.waterServing_task.add_task('Return Glass')
-      self.placeGlass_subtask = self.waterServing_task.add_task('Place Glass')
+    self.waterServing_task = self.tasklist.add_task('WaterServing')
+    self.findGlass_subtask = self.waterServing_task.add_task('Find Glass')
+    self.graspGlass_subtask = self.waterServing_task.add_task('Grasp Glass')
+    self.liftGlass_subtask = self.waterServing_task.add_task('Lift Glass')
+    self.drinkGlass_subtask = self.waterServing_task.add_task('Drink Glass')
+    self.returnGlass_subtask = self.waterServing_task.add_task('Return Glass')
+    self.placeGlass_subtask = self.waterServing_task.add_task('Place Glass')
 
 
 
   def initSimple(self):
 
-      self.NUM_UPDATE_CALLS = 30
-      self.Initialized = False
+    self.NUM_UPDATE_CALLS = 30
+    self.Initialized = False
 
-      rospy.init_node('bite_serving_scenario', anonymous = True)
-      env_path = '/environments/table.env.xml'
+    rospy.init_node('bite_serving_scenario', anonymous = True)
+    env_path = '/environments/table.env.xml'
 
-      openravepy.RaveInitialize(True, level=openravepy.DebugLevel.Debug)
-      openravepy.misc.InitOpenRAVELogging();
-      self.env, self.robot = adapy.initialize(attach_viewer='qtcoin', sim=False, env_path = env_path)
-      #embed()
+    openravepy.RaveInitialize(True, level=openravepy.DebugLevel.Debug)
+    openravepy.misc.InitOpenRAVELogging();
+    self.env, self.robot = adapy.initialize(attach_viewer='qtcoin', sim=False, env_path = env_path)
+    #embed()
 
-      self.manip = self.robot.arm
-      inds, pos = self.robot.configurations.get_configuration('home')
-      pos[1] = -1.57;
-      pos[2] = 0;
-      self.robot.SetDOFValues(self.robot.GetDOFValues())
+    self.manip = self.robot.arm
+    inds, pos = self.robot.configurations.get_configuration('home')
+    pos[1] = -1.57;
+    pos[2] = 0;
+    self.robot.SetDOFValues(self.robot.GetDOFValues())
 
-      # find the ordata
-      rospack = rospkg.RosPack()
-      file_root = rospack.get_path('pr_ordata')
+    # find the ordata
+    rospack = rospkg.RosPack()
+    file_root = rospack.get_path('pr_ordata')
 
-      self.table = self.env.GetKinBody('table')
+    self.table = self.env.GetKinBody('table')
 
-      robot_pose = numpy.array([[1, 0, 0, 0.409],[0, 1, 0, 0.338],[0, 0, 1, 0.795],[0, 0, 0, 1]])
-      self.robot.SetTransform(robot_pose)
+    robot_pose = numpy.array([[1, 0, 0, 0.409],[0, 1, 0, 0.338],[0, 0, 1, 0.795],[0, 0, 0, 1]])
+    self.robot.SetTransform(robot_pose)
 
-      ViewSide1Obj = self.env.GetKinBody('ViewSide1')
-      ViewSide1Trans = ViewSide1Obj.GetTransform()
+    ViewSide1Obj = self.env.GetKinBody('ViewSide1')
+    ViewSide1Trans = ViewSide1Obj.GetTransform()
 
-      ViewSide2Obj = self.env.GetKinBody('ViewSide2')
-      ViewSide2Trans = ViewSide2Obj.GetTransform()
+    ViewSide2Obj = self.env.GetKinBody('ViewSide2')
+    ViewSide2Trans = ViewSide2Obj.GetTransform()
 
-      ViewTopObj = self.env.GetKinBody('ViewTop')
-      ViewTopTrans = ViewTopObj.GetTransform()
+    ViewTopObj = self.env.GetKinBody('ViewTop')
+    ViewTopTrans = ViewTopObj.GetTransform()
 
-      self.numOfGrasps = 0
-      self.numOfUpdateCalls = 100
-
-
-      viewer = self.env.GetViewer()
-
-      self.trajectoryPlanned = True
+    self.numOfGrasps = 0
+    self.numOfUpdateCalls = 100
 
 
-      # tf listener for querying transforms
-      self.tfListener = tf.TransformListener()
+    viewer = self.env.GetViewer()
 
-      self.manip = self.robot.arm
-      #self.robot.SetActiveManipulator(manip)
-      activedofs = [i for i in range(6)]
-      self.robot.SetActiveDOFs(activedofs)
-      self.robot.planner = prpy.planning.Sequence(self.robot.cbirrt_planner)      
+    self.trajectoryPlanned = True
 
 
-      self.Initialized = True
+    # tf listener for querying transforms
+    self.tfListener = tf.TransformListener()
 
-      self.pub = rospy.Publisher('ada_tasks',String, queue_size=10)
-      self.sub = rospy.Subscriber("/perception/morsel_detection", String, self._MorselDetectionCallback, queue_size=1)
+    self.manip = self.robot.arm
+    #self.robot.SetActiveManipulator(manip)
+    activedofs = [i for i in range(6)]
+    self.robot.SetActiveDOFs(activedofs)
+    self.robot.planner = prpy.planning.Sequence(self.robot.cbirrt_planner)      
 
-      self.tasklist = TaskLogger()  
-      self.addWaterServingTask()
 
-      self.ball=self.env.ReadKinBodyURI('objects/smallsphere.kinbody.xml')
-      self.ball.SetName('smallsphere')
-      self.env.Add(self.ball)
+    self.Initialized = True
 
-      self.manip = self.robot.arm
-      #self.manip.SetIKSolver(iksolver)
+    self.pub = rospy.Publisher('ada_tasks',String, queue_size=10)
+    self.sub = rospy.Subscriber("/perception/morsel_detection", String, self._MorselDetectionCallback, queue_size=1)
 
-      self.statePub = rospy.Publisher('ROBOT_STATE', String, queue_size=10)
-      #rospy.init_node('Robot State')
-      self.rospyRate = rospy.Rate(33.33) # 10hz
+    self.tasklist = TaskLogger()  
+    self.addWaterServingTask()
 
-      self.ROBOT_STATE = "EXECUTING_TRAJECTORY"
-      self.statePub.publish(adaManipulationTester.ROBOT_STATE)
+    self.ball=self.env.ReadKinBodyURI('objects/smallsphere.kinbody.xml')
+    self.ball.SetName('smallsphere')
+    self.env.Add(self.ball)
 
-      self.manip.PlanToConfiguration(lookingAtFaceConfiguration)
-      time.sleep(3)
+    self.manip = self.robot.arm
+    #self.manip.SetIKSolver(iksolver)
 
-      self.bite_detected = False
+    self.statePub = rospy.Publisher('ROBOT_STATE', String, queue_size=10)
+    #rospy.init_node('Robot State')
+    self.rospyRate = rospy.Rate(33.33) # 10hz
 
-      self.ROBOT_STATE = "LOOKING_AT_FACE"
-      self.statePub.publish(adaManipulationTester.ROBOT_STATE)
+    self.ROBOT_STATE = "EXECUTING_TRAJECTORY"
+    self.statePub.publish(adaManipulationTester.ROBOT_STATE)
+
+    self.manip.PlanToConfiguration(lookingAtFaceConfiguration)
+    time.sleep(3)
+
+    self.bite_detected = False
+
+    self.ROBOT_STATE = "LOOKING_AT_FACE"
+    self.statePub.publish(adaManipulationTester.ROBOT_STATE)
 
   def lookingAtPlate(self):
-      if(self.bite_detected == True):
-          print "registering ball pose"
-          self.ball.SetTransform(self.bite_world_pose)
-          self.ROBOT_STATE = "EXECUTING_TRAJECTORY"
+    if(self.bite_detected == True):
+      print "registering ball pose"
+      self.ball.SetTransform(self.bite_world_pose)
+      self.ROBOT_STATE = "EXECUTING_TRAJECTORY"
 
   def lookingAtFace(self):
-
-
-      #get face recognition
-      self.bite_detected = False
-      self.ROBOT_STATE = "EXECUTING_TRAJECTORY"
-      self.statePub.publish(adaManipulationTester.ROBOT_STATE)
-      self.manip.PlanToConfiguration(lookingAtPlateConfiguration)
-      time.sleep(3)
-      self.ROBOT_STATE = "LOOKING_AT_PLATE"
-      self.statePub.publish(adaManipulationTester.ROBOT_STATE)
+    #get face recognition
+    self.bite_detected = False
+    self.ROBOT_STATE = "EXECUTING_TRAJECTORY"
+    self.statePub.publish(adaManipulationTester.ROBOT_STATE)
+    self.manip.PlanToConfiguration(lookingAtPlateConfiguration)
+    time.sleep(3)
+    self.ROBOT_STATE = "LOOKING_AT_PLATE"
+    self.statePub.publish(adaManipulationTester.ROBOT_STATE)
 
   def _MorselDetectionCallback(self, msg):
-      obj =  json.loads(msg.data)
-      arr = obj['pts3d']
-      pos = np.asarray(arr)
-      #embed()
-      if(pos is None) or(len(pos)==0) or (self.ROBOT_STATE!="LOOKING_AT_PLATE"):
-          return
-      else:
-        #self.plateDetectedTimes = self.plateDetectedTimes + 1
-        relative_pos = pos[0]
-        relative_pose = np.eye(4)
-        relative_pose[0,3] = relative_pos[0] 
-        relative_pose[1,3] = relative_pos[1]
-        relative_pose[2,3] = relative_pos[2]
-        world_camera = self.robot.GetLinks()[7].GetTransform()
-        self.bite_world_pose = np.dot(world_camera,relative_pose)
-        self.bite_detected = True
+    obj =  json.loads(msg.data)
+    arr = obj['pts3d']
+    pos = np.asarray(arr)
+    #embed()
+    if(pos is None) or(len(pos)==0) or (self.ROBOT_STATE!="LOOKING_AT_PLATE"):
+      return
+    else:
+      #self.plateDetectedTimes = self.plateDetectedTimes + 1
+      relative_pos = pos[0]
+      relative_pose = np.eye(4)
+      relative_pose[0,3] = relative_pos[0] 
+      relative_pose[1,3] = relative_pos[1]
+      relative_pose[2,3] = relative_pos[2]
+      world_camera = self.robot.GetLinks()[7].GetTransform()
+      self.bite_world_pose = np.dot(world_camera,relative_pose)
+      self.bite_detected = True
 
 
         
 
   def executeTrajectory(self):    
-
-          defaultVelocityLimits = self.robot.GetDOFVelocityLimits()
-
-
-          self.robot.planner = prpy.planning.Sequence(self.robot.cbirrt_planner) 
-
-          endEffectorPose = defaultEndEffectorPose
-          endEffectorPose[0,3] = self.bite_world_pose[0,3]-0.08
-          endEffectorPose[1,3] = self.bite_world_pose[1,3]+0.01
-          self.manip.PlanToEndEffectorPose(endEffectorPose)
-          time.sleep(3)
-
-
-          self.manip.PlanToEndEffectorOffset([0,0,-1],0.22)
-          time.sleep(2)
-          self.manip.PlanToEndEffectorOffset([0,0,1],0.22)
-          time.sleep(2)
-          self.manip.PlanToConfiguration(servingConfiguration)
-          time.sleep(6)
-
-  
-          print str(self.tasklist)
-          if(self.waterServing_task.is_complete()):
-            self.addWaterServingTask()
-            self.pub.publish(str(self.tasklist))
-          self.ROBOT_STATE = "LOOKING_AT_FACE"
+    defaultVelocityLimits = self.robot.GetDOFVelocityLimits()
+    self.robot.planner = prpy.planning.Sequence(self.robot.cbirrt_planner) 
+    endEffectorPose = defaultEndEffectorPose
+    endEffectorPose[0,3] = self.bite_world_pose[0,3]-0.08
+    endEffectorPose[1,3] = self.bite_world_pose[1,3]+0.01
+    self.manip.PlanToEndEffectorPose(endEffectorPose)
+    time.sleep(3)
+    self.manip.PlanToEndEffectorOffset([0,0,-1],0.22)
+    time.sleep(2)
+    self.manip.PlanToEndEffectorOffset([0,0,1],0.22)
+    time.sleep(2)
+    self.manip.PlanToConfiguration(servingConfiguration)
+    time.sleep(6)
+    print str(self.tasklist)
+    if(self.waterServing_task.is_complete()):
+      self.addWaterServingTask()
+      self.pub.publish(str(self.tasklist))
+    self.ROBOT_STATE = "LOOKING_AT_FACE"
   
 if __name__ == "__main__":
-    adaManipulationTester = ADAmanipulationTester()
-    while not rospy.is_shutdown():
-      if(adaManipulationTester.ROBOT_STATE == "INITIAL"):
-          adaManipulationTester.initSimple()
-      elif(adaManipulationTester.ROBOT_STATE == "LOOKING_AT_FACE"):
-          adaManipulationTester.lookingAtFace()
-      elif(adaManipulationTester.ROBOT_STATE == "LOOKING_AT_PLATE"):
-          adaManipulationTester.lookingAtPlate()
-      elif(adaManipulationTester.ROBOT_STATE == "EXECUTING_TRAJECTORY"):
-          adaManipulationTester.executeTrajectory()
-      else:
-          print "Error: Unknown ROBOT_STATE"
-
-
-      adaManipulationTester.statePub.publish(adaManipulationTester.ROBOT_STATE)
-      adaManipulationTester.rospyRate.sleep()
+  adaManipulationTester = ADAmanipulationTester()
+  while not rospy.is_shutdown():
+    if(adaManipulationTester.ROBOT_STATE == "INITIAL"):
+      adaManipulationTester.initSimple()
+    elif(adaManipulationTester.ROBOT_STATE == "LOOKING_AT_FACE"):
+      adaManipulationTester.lookingAtFace()
+    elif(adaManipulationTester.ROBOT_STATE == "LOOKING_AT_PLATE"):
+      adaManipulationTester.lookingAtPlate()
+    elif(adaManipulationTester.ROBOT_STATE == "EXECUTING_TRAJECTORY"):
+      adaManipulationTester.executeTrajectory()
+    else:
+      print "Error: Unknown ROBOT_STATE"
+  adaManipulationTester.statePub.publish(adaManipulationTester.ROBOT_STATE)
+  adaManipulationTester.rospyRate.sleep()
