c82e91b893296e2ee3d7740157df8d457774cb8b
Stefanos Nikolaidis <snikolaidis1983@gmail.com>
2016-02-24 18:49:49 -0500

    Updated setup.py with correct directory

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 package_info = generate_distutils_setup()
 package_info['packages'] = ['ada_meal_scenario', 
                             'ada_meal_scenario.actions']
-package_info['package_dir'] = {'ada_meal_scenario':'src/adapy'}
+package_info['package_dir'] = {'ada_meal_scenario':'src'}
 package_info['install_requires'] = []
 
 setup(**package_info)
