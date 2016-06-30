c8f3310a7ece2585d229418ff13a5fe0350cf944
Shervin Javdani <sjavdani@cmu.edu>
2016-06-30 11:36:22 -0400

    added assistance policy action to swap to shared autonomy or direct teleop

---
+++ src/ada_meal_scenario/actions/assistance_policy_action.py
@@ -0,0 +1,33 @@
+#creates a shared autonomy policy that runs essentially as an action
+
+import logging, numpy, prpy, os
+from ada_assistance_policy.AdaHandler import AdaHandler
+from ada_assistance_policy.Goal import Goal
+import rospy
+import rospkg
+
+from bypassable_action import ActionException, BypassableAction
+
+#import prpy.rave, prpy.util
+#from prpy.planning.base import PlanningError
+#from prpy.exceptions import TrajectoryNotExecutable
+#import time
+
+
+
+
+class AssistancePolicyAction(BypassableAction):
+    def __init__(self, bypass=False):
+        BypassableAction.__init__(self, 'ASSISTANCE_POLICY', bypass=bypass)
+        
+    def _run(self, manip, objects, desired_ee_poses):
+        robot = manip.GetRobot()
+        env = robot.GetEnv()
+
+        all_goals = [Goal(obj.GetTransform(), [desired_ee_pose]) for obj, desired_ee_pose in zip(objects, desired_ee_poses)]
+        ada_handler = AdaHandler(env, robot, all_goals, objects)
+        ada_handler.execute_policy(simulate_user=False, fix_magnitude_user_command=False)
+        #ada_handler.execute_direct_teleop(simulate_user=False)
+
+
+
