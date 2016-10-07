5209c17bf6e0b42f05f0480f8e8a48c84cfa093c
Shervin Javdani <sjavdani@cmu.edu>
2016-10-07 18:24:39 -0400

    removed pupil tracking

--- scripts/runBiteServing.py
+++ scripts/runBiteServing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import adapy, argparse, logging, numpy, os, sys, openravepy, prpy, rospy, random
+import adapy, argparse, logging, numpy, os, sys, openravepy, prpy, rospy, random, rospkg
 from prpy.planning.base import PlanningError
 from catkin.find_in_workspaces import find_in_workspaces
 from ada_meal_scenario.actions.bite_serving import BiteServing
@@ -20,7 +20,7 @@ from prpy.tsr.rodrigues import *
 
 from ada_meal_scenario.gui_handler import *
 
-from gazetracking.pupil_capture import PupilCapture
+#from gazetracking.pupil_capture import PupilCapture
 
 import warnings
 warnings.simplefilter(action = "ignore", category = FutureWarning)
@@ -88,7 +88,6 @@ def setup(sim=False, viewer=None, debug=True):
         robot.SetDOFValues(dofindices=indices, values=values)
     else:
         robot.arm.PlanToNamedConfiguration('ada_meal_scenario_servingConfiguration')
-    #    robot.SetDOFValues(startConfig)
 
     logger.info('Initial Config Set')
     # Load the fork into the robot's hand
@@ -112,9 +111,7 @@ def setup(sim=False, viewer=None, debug=True):
                             [ 0.,  1., 0., y_trans_tool],
                             [ 0.,  0.,  1., -0.042],
                             [ 0.,  0.,  0., 1.]])
-    rotate_tool_in_ee = rodrigues([0., 0., 0.])
     rotate_tool_in_ee = rodrigues([0., 0., np.pi/32.])
-    #rotate_tool_in_ee = rodrigues([0., 0., -np.pi/32.])
     tool_in_ee[0:3, 0:3] = np.dot(rotate_tool_in_ee, tool_in_ee[0:3, 0:3])
 
     tool_in_world = numpy.dot(ee_in_world, tool_in_ee)
@@ -135,12 +132,13 @@ def setup(sim=False, viewer=None, debug=True):
     fork_in_world = numpy.dot(ee_in_world, fork_in_ee)
     fork.SetTransform(fork_in_world)
 
-    logger.info('creating fork and tool boxes')
 
+    # since there were problems with the meshes and collision checking, 
+    # these boxes were added around the objects. They help make sure 
+    # the robot avoids collisions of the fork while it is grabbed
     fork_box = make_collision_box_body(fork, add_to_pos=np.array([0.0, 0.0, 0.05]), add_to_extents=np.array([0.02, 0.02, 0.1]))
     tool_box = make_collision_box_body(tool, add_to_pos=np.array([0.0, 0.0, 0.04]), add_to_extents=np.array([0.055, 0.055, 0.055]))
 
-    logger.info('fork and tool boxes created')
 
     
     #find all finger links
@@ -160,18 +158,6 @@ def setup(sim=False, viewer=None, debug=True):
     robot.Grab(tool_box, grablink=grab_link, linkstoignore=finger_link_inds)
     robot.Grab(fork_box, grablink=grab_link, linkstoignore=finger_link_inds)
 
-#    print 'grab link: ' + str(grab_link)
-#    print 'links to ignore: ' + str(finger_link_inds)
-#
-#    print 'grabbed name: ' + str(robot.GetGrabbedInfo()[0]._grabbedname)
-#    print 'ee name: ' + str(robot.GetGrabbedInfo()[0]._robotlinkname)
-#    print 'links ignored' + str(robot.GetGrabbedInfo()[0]._setRobotLinksToIgnore)
-
-
-    # Set serving phrases
-    serving_phrases = ['That looks like a delicious bite ', 
-                        'Here you go, I hope you enjoy it ',
-                        'That was a good choice ']
 
     # add boxes for constraint to not hit user
     KinovaStudyHelpers.AddConstraintBoxes(env, robot)
@@ -213,74 +199,6 @@ def make_collision_box_body(kinbody, add_to_pos=np.array([0.0, 0.0, 0.0]), add_t
   
 
 
-
-
-def pose_to_arrow_markers(pose, ns='axes', id_start=0, lifetime_secs=10):
-  markers = []
-  scale_size = 0.1
-  dims = [.01, .02, 0.1]
-  dirs = []
-  dirs.append(np.array([1.,0.,0.]))
-  dirs.append(np.array([0.,1.,0.]))
-  dirs.append(np.array([0.,0.,1.]))
-  for ind,dir in enumerate(dirs):
-    marker = Marker()
-    marker.header.frame_id = "map"
-    marker.header.stamp = rospy.Time.now()
-    marker.type = Marker.ARROW
-    marker.action = Marker.ADD;
-    marker.id = id_start + ind
-    marker.lifetime.secs=lifetime_secs
-    marker.ns = ns
-
-    marker.scale.x = dims[0]
-    marker.scale.y = dims[1]
-    marker.scale.z = dims[2]
-
-    marker.pose.position.x = 0.
-    marker.pose.position.y = 0.
-    marker.pose.position.z = 0.
-    marker.pose.orientation.x = 0.
-    marker.pose.orientation.y = 0.
-    marker.pose.orientation.z = 0.
-    marker.pose.orientation.w = 1.
-
- 
-
-    start_pt = pose[0:3,3]
-    end_pt = np.dot(pose[0:3,0:3], dir)*scale_size + start_pt
-    marker.points = [np_array_to_point(start_pt), np_array_to_point(end_pt)]
-
-    marker.color.r = dir[0]
-    marker.color.g = dir[1]
-    marker.color.b = dir[2]
-    marker.color.a = 1.0
-
-    markers.append(marker)
-
-  return markers
-
-def np_array_to_point(pt_np):
-  pt = Point()
-  pt.x = pt_np[0]
-  pt.y = pt_np[1]
-  pt.z = pt_np[2]
-  return pt
-
-def joystick_callback(data):
-    global joystick_go_signal
-    # If the left button is pressed, update go signal flag
-    if data.buttons[0]:
-        joystick_go_signal = True
-
-#def tasks_callback(data):
-#    global serving_phrases
-#    # Announce the bite!
-#    if data.data == 'SERVING':
-#        # Randomly select one of the serving phrases
-#        phrase = random.choice(serving_phrases) + username
-#        robot.Say(phrase)
-
 def ResetTrial(robot):
   logger.info('Resetting Robot')
   if robot.simulated:
@@ -299,8 +217,6 @@ def ResetTrial(robot):
 
 
 if __name__ == "__main__":
-    global joystick_go_signal, robot, username
-
     state_pub = rospy.Publisher('ada_tasks',String, queue_size=10)
         
     rospy.init_node('bite_serving_scenario', anonymous=True)
@@ -312,7 +228,7 @@ if __name__ == "__main__":
     parser.add_argument("--detection-sim", action="store_true", help="Simulate detection of morsal")
     parser.add_argument("--jaco", action="store_true", default=False, help="Using jaco robot")
     parser.add_argument("--userid", type=int, help="User ID number")
-    parser.add_argument("--no-pupil-tracking", action="store_true", help="Disable pupil tracking")
+#    parser.add_argument("--no-pupil-tracking", action="store_true", help="Disable pupil tracking")
     args = parser.parse_args(rospy.myargv()[1:]) # exclude roslaunch args
 
     sim = not args.real
@@ -320,47 +236,18 @@ if __name__ == "__main__":
 
     gui_get_event, gui_trial_starting_event, gui_queue, gui_process = start_gui_process()
 
-    #slow robot down
-    #save old limits
-#    old_acceleration_limits = robot.GetDOFAccelerationLimits()
-#    old_velocity_limits = robot.GetDOFVelocityLimits()
-#
-#    #slow down robot
-#    #robot.SetDOFVelocityLimits(0.6*robot.GetDOFVelocityLimits())
-#    #robot.SetDOFAccelerationLimits(0.8*robot.GetDOFAccelerationLimits())
-#
-#    robot.SetDOFVelocityLimits(old_velocity_limits)
-#    robot.SetDOFAccelerationLimits(old_acceleration_limits)
-# 
-
-    #start by going to ada_meal_scenario_servingConfiguration
-    if sim:
-        indices, values = robot.configurations.get_configuration('ada_meal_scenario_servingConfiguration')
-        robot.SetDOFValues(dofindices=indices, values=values)
-    else:
-        robot.PlanToNamedConfiguration('ada_meal_scenario_servingConfiguration', execute=True)
-
-#    camera_link = robot.GetLink('Camera_RGB_Frame')
-#    camera_transform = camera_link.GetTransform()
-#    with prpy.viz.RenderPoses([camera_transform], env):
-
-    # Subscribe to the 'ada_tasks' topic (for talking during certain tasks)
-    #task_listener = rospy.Subscriber('ada_tasks', String, tasks_callback)
-
-
     # Start eyetracker remote controller
-    do_pupil_tracking= not args.no_pupil_tracking
-    if do_pupil_tracking:
-        logger.info('setting up pupil tracking')
-        pupil_capture = PupilCapture()
-        pupil_capture.setup(logger)
-    else:
-        pupil_capture = None
+#    do_pupil_tracking= not args.no_pupil_tracking
+#    if do_pupil_tracking:
+#        logger.info('setting up pupil tracking')
+#        pupil_capture = PupilCapture()
+#        pupil_capture.setup(logger)
+#    else:
+#        pupil_capture = None
+#    logger.info('pupil tracker set, or none needed')
 
-    logger.info('pupil tracker set, or none needed')
     # Where to store rosbags and other user data - set this manually if userid was provided,
     # otherwise dynamically generate it
-    from ada_teleoperation.DataRecordingUtils import *
     file_directory_user = None
     file_directory = rospkg.RosPack().get_path('ada_meal_scenario') + '/trajectory_data'
     if args.userid:
@@ -375,7 +262,8 @@ if __name__ == "__main__":
         else:
             os.makedirs(file_directory_user)
     else:
-        user_number, file_directory_user = get_next_available_user_ind(file_directory=file_directory, user_folder_base=user_folder_base_default)
+        from ada_teleoperation.DataRecordingUtils import get_next_available_user_ind
+        user_number, file_directory_user = get_next_available_user_ind(file_directory=file_directory)
 
     while True:
         empty_queue(gui_queue)
@@ -389,10 +277,10 @@ if __name__ == "__main__":
         if gui_return['quit']:
             break
         elif gui_return['start']:
-            if gui_return['record']:
-                # Start eyetracker recording
-                if pupil_capture:
-                    pupil_capture.start()
+#            if gui_return['record']:
+#                # Start eyetracker recording
+#                if pupil_capture:
+#                    pupil_capture.start()
             #tell gui we are starting to reset next trial
             gui_trial_starting_event.set()
             # Start bite collection and presentation
@@ -405,10 +293,9 @@ if __name__ == "__main__":
 
                 ResetTrial(robot)
 
-            finally:
-                if pupil_capture:
-                    pupil_capture.stop()
-
+#            finally:
+#                if pupil_capture:
+#                    pupil_capture.stop()
 
 
     gui_process.terminate()
